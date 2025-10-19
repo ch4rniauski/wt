9ed9e7731b02c87bb52ae8a3384ff6732cbba0be
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-10-19 11:27:52 +0300

    Add laba18

--- laba17/src/app.module.ts
+++ laba17/src/app.module.ts
@@ -12,11 +12,11 @@ import { RolesModule } from './roles/roles.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '5432',
-      database: 'Laba16',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT!,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
